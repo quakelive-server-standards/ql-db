ac113872597ab8a8208899443112ba9d3d4c9a86
René Gastmeier <rene@coderitter.io>
2021-05-15 15:22:04 +0200

    Rename testCrud.ts to tools.ts and add containsMisfit

--- test/tools.ts
+++ test/tools.ts
@@ -1,6 +1,7 @@
 import { Criteria, ReadCriteria } from 'knight-criteria'
 import { count as crudCount, create as crudCreate, read as crudRead } from 'knight-orm'
 import { PgTransaction } from 'knight-pg-transaction'
+import { Misfit } from 'knight-validation'
 import schema from '../src/domain/DbSchema'
 import Services from '../src/Services'
 
@@ -24,4 +25,14 @@ export async function queryFn(sqlString: string, values?: any[]) {
   let tx = new PgTransaction(Services.get().pool)
   let result = await tx.query(sqlString, values)
   return result.rows
+}
+
+export function containsMisfit(field: string, misfitType: string, misfits: Misfit[]): boolean {
+  for (let misfit of misfits) {
+    if (misfit.field == field && misfit.name == misfitType) {
+      return true
+    }
+  }
+
+  return false
 }
